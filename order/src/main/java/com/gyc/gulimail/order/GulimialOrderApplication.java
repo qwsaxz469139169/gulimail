e8a2e66439ad9a665967f5f5ea0d624af670c744
Yuchen Guo <qwsaxz469139169@gmail.com>
2020-12-29 10:39:36 +0800

    begin

---
+++ order/src/main/java/com/gyc/gulimail/order/GulimialOrderApplication.java
@@ -0,0 +1,13 @@
+package com.gyc.gulimail.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimialOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimialOrderApplication.class, args);
+    }
+
+}
---
+++ order/src/test/java/com/gyc/gulimail/order/GulimialOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyc.gulimail.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimialOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
